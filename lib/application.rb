260ad7283cf875320d49be59880e03433d8e0821
eparlato <edoardoparlato@nebulab.com>
2024-05-06 22:40:29 +0200

    Add first user story description with failing e2e test.

--- lib/application.rb
+++ lib/application.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Application
-  def works?
-    true
+  def run_with_file(file_path)
+    "0"
   end
 end
---
+++ spec/e2e/application_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "Application"
+
+RSpec.describe Application do
+  context "when it reads an input file" do
+    xit "returns a set of account numbers" do
+      expected_output =
+        "000000000\n" \
+        "111111111\n" \
+        "123456789\n"
+
+      expect(Application.new.run_with_file("input/input_file.txt")).to equal(expected_output)
+    end
+  end
+
+end
